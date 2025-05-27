cfc45ba40fedace5e22c2a3345e83f93cb12a992
Jeayeong_Hong <hong612644@gmail.com>
2025-05-27 19:55:41 +0900

    UI/UX개선

--- src/components/StartPage.jsx
+++ src/components/StartPage.jsx
@@ -2,9 +2,9 @@ import { Link } from 'react-router-dom';
 
 function StartPage() {
   return (
-    <div>
-      <h1>개발자 유형 테스트</h1>
-      <Link to="/question">시작하기</Link>
+    <div className="start-container">
+      <h1 className="start-title">개발자 유형 테스트</h1>
+      <a className="start-button" href="/question">시작하기</a>
     </div>
   );
 }
