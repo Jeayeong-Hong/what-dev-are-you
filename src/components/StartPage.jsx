623b36f54bf16f3695abee9e0f75e57f87dce26c
Jeayeong_Hong <hong612644@gmail.com>
2025-05-27 16:53:14 +0900

    fix: move index.html to root for Vite entry

--- src/components/StartPage.jsx
+++ src/components/StartPage.jsx
@@ -1,9 +1,12 @@
+import { Link } from 'react-router-dom';
+
 function StartPage() {
   return (
     <div>
-        <h1>개발자 유형 테스트</h1>
-        <a href="/question">시작하기</a>
+      <h1>개발자 유형 테스트</h1>
+      <Link to="/question">시작하기</Link>
     </div>
   );
 }
+
 export default StartPage;
